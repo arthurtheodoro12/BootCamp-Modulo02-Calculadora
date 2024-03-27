c6a5da2bc55df4bf0bb26242112cda998d089b51
Arthur Theodoro <arthur.theodoro01@fatec.sp.gov.br>
2024-03-27 16:31:40 -0300

    closes #4 - Implementar as 4 operações matemáticas

--- javascript.js
+++ javascript.js
@@ -1,7 +1,22 @@
-function Somar() {
+function calcular(operacao) {
     var Num1 = parseFloat(document.getElementById("num1").value);
     var Num2 = parseFloat(document.getElementById("num2").value);
+    var Resultado
 
-    var Resultado = Num1 + Num2;
-    document.getElementById("resultado").value = Resultado;
+    if(operacao === 'soma') {
+        Resultado = Num1 + Num2;
+    } else if(operacao === 'sub') {
+        Resultado = Num1 - Num2;
+    } else if(operacao === 'multi') {
+        Resultado = Num1 * Num2
+    } else if(operacao === "div") {
+        if(Num2 == 0) {
+            alert("Não é possível dividir por 0!")
+            document.getElementById("num2").focus()
+        } else {
+            Resultado = Num1/Num2
+        }
+    }
+
+    document.getElementById("resultado").value = Resultado.toFixed(2);
 }
