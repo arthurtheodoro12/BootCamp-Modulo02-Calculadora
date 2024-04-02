7e418c62fe48b549e5990b067a4d4b63a3159b73
Arthur Theodoro <arthur.theodoro01@fatec.sp.gov.br>
2024-04-02 15:43:07 -0300

    closes #5, closes #6 - Historico

--- javascript.js
+++ javascript.js
@@ -1,22 +1,33 @@
 function calcular(operacao) {
     var Num1 = parseFloat(document.getElementById("num1").value);
     var Num2 = parseFloat(document.getElementById("num2").value);
-    var Resultado
+    var Resultado;
 
-    if(operacao === 'soma') {
+    if(operacao === '+') {
         Resultado = Num1 + Num2;
-    } else if(operacao === 'sub') {
+    } else if(operacao === '-') {
         Resultado = Num1 - Num2;
-    } else if(operacao === 'multi') {
-        Resultado = Num1 * Num2
-    } else if(operacao === "div") {
+    } else if(operacao === '*') {
+        Resultado = Num1 * Num2;
+    } else if(operacao === "/") {
         if(Num2 == 0) {
-            alert("Não é possível dividir por 0!")
-            document.getElementById("num2").focus()
+            alert("Não é possível dividir por 0!");
+            document.getElementById("num2").focus();
+            return;
         } else {
-            Resultado = Num1/Num2
+            Resultado = Num1 / Num2;
         }
     }
 
     document.getElementById("resultado").value = Resultado.toFixed(2);
+
+    let History = document.getElementById("historico");
+    let newHistoryElement = document.createElement("div");
+    newHistoryElement.innerHTML = Num1 + " " + operacao + " " + Num2 + " = " + Resultado.toFixed(2);
+    History.insertBefore(newHistoryElement, History.firstChild);
+
+    if(History.children.length > 10) {
+        History.removeChild(History.childNodes[10]);
+    }
 }
+
